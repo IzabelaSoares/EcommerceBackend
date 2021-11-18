30d607336dadfb8af61eaa0bde02611841aa164e
IzabelaSoares <soares.izabelamaria@gmail.com>
2021-11-18 11:31:21 -0300

    Criação da base de dados automatica e database h2 de testes

---
+++ src/main/java/com/treinamento/EcommerceBackend/domain/Category.java
@@ -0,0 +1,64 @@
+package com.treinamento.EcommerceBackend.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    public Category() {
+    }
+
+    public Category(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "nome='" + nome + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
